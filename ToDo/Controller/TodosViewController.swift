6333fb3734a9979f21d66db17f7401807319bc4d
sivanpayyadakath <sivan.payyadakath@gmail.com>
2019-05-20 14:49:20 +0900

    section and deleting implemented #1 #2

--- ToDo/Controller/TodosViewController.swift
+++ ToDo/Controller/TodosViewController.swift
@@ -14,12 +14,15 @@ class TodosViewController: UIViewController {
     @IBOutlet weak var addButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
     var todos: [NSManagedObject] = []
+    var doing: [NSManagedObject] = []
+    var done: [NSManagedObject] = []
     var sections: [NSManagedObject] = []
-    var p: NSSet = []
+    let titleForSections = ["Not Started", "On Progress", "Done"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.tableView.isEditing = true
         tableView.rowHeight = UITableView.automaticDimension
 //        tableView.estimatedRowHeight = 80
         
@@ -31,11 +34,26 @@ class TodosViewController: UIViewController {
 
         let managedView = appDelegate.persistentContainer.viewContext
 
-        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Todo")
-
+        let todofetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Todo")
+        let doingfetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Todo")
+        let donefetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Todo")
+        
+        let predicate = NSPredicate(format: "status == %d",0)
+        todofetchRequest.predicate = predicate
+        
+        let doingpredicate = NSPredicate(format: "status == %d",1)
+        doingfetchRequest.predicate = doingpredicate
+        
+        let donepredicate = NSPredicate(format: "status == %d",2)
+        donefetchRequest.predicate = donepredicate
+        
+        
         do {
-            try todos = managedView.fetch(fetchRequest)
-        } catch let error as NSError {
+            try todos = managedView.fetch(todofetchRequest)
+            try doing = managedView.fetch(doingfetchRequest)
+            try done = managedView.fetch(donefetchRequest)
+            
+            } catch let error as NSError {
             print("couldnt fetch \(error) \(error.userInfo)")
         }
         
@@ -137,6 +155,7 @@ class TodosViewController: UIViewController {
         
         todo.setValue(title, forKey: "title")
         todo.setValue(sub, forKey: "sub")
+        todo.setValue(1, forKey: "status")
         
         do {
             try managedContext.save()
@@ -159,9 +178,23 @@ class TodosViewController: UIViewController {
             
             let managedContext = appDelegate.persistentContainer.viewContext
             
-            let itemToDelete = todos.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .right)
-            managedContext.delete(itemToDelete)
+            switch(indexPath.section){
+            case 0:
+                let itemToDelete = todos.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .right)
+                managedContext.delete(itemToDelete)
+            case 1:
+                let itemToDelete = doing.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .right)
+                managedContext.delete(itemToDelete)
+            case 2:
+                let itemToDelete = done.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .right)
+                managedContext.delete(itemToDelete)
+            default:
+                print("error in deleting")
+            }
+            
         
         }
         
@@ -171,23 +204,90 @@ class TodosViewController: UIViewController {
 }
 
 extension TodosViewController : UITableViewDataSource {
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return todos.count
+        var k: Int = 0
+        switch section {
+        case 0:
+            k = todos.count
+        case 1:
+            k = doing.count
+        case 2:
+            k = done.count
+        default:
+            print("error")
+        }
+        return k
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return sections.count + 1
+        return 3
     }
+
     
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return titleForSections[section]
+    }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let todo = todos[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TodosTableViewCell
-//        cell.textLabel?.text = todo.value(forKey: "title") as? String
-        cell.todoTitle?.text = todo.value(forKey: "title") as? String
-//        cell.todoSubTitle
-        cell.todoSubTitle?.text = todo.value(forKey: "sub") as? String
+        switch indexPath.section {
+        case 0:
+            let todo = todos[indexPath.row]
+            cell.todoTitle?.text = todo.value(forKey: "title") as? String
+            cell.todoSubTitle?.text = todo.value(forKey: "sub") as? String
+        case 1:
+            let todo = doing[indexPath.row]
+            cell.todoTitle?.text = todo.value(forKey: "title") as? String
+            cell.todoSubTitle?.text = todo.value(forKey: "sub") as? String
+        case 2:
+            let todo = done[indexPath.row]
+            cell.todoTitle?.text = todo.value(forKey: "title") as? String
+            cell.todoSubTitle?.text = todo.value(forKey: "sub") as? String
+        default:
+            print("error")
+        }
+
         return cell
     }
+
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        var itemToMove: NSManagedObject?
+
+        switch sourceIndexPath.section {
+        case 0:
+            itemToMove = todos[sourceIndexPath.row]
+            todos.remove(at: sourceIndexPath.row)
+        case 1:
+            itemToMove = doing[sourceIndexPath.row]
+            doing.remove(at: sourceIndexPath.row)
+        case 2:
+            itemToMove = done[sourceIndexPath.row]
+            done.remove(at: sourceIndexPath.row)
+        default:
+            print("error in fetching item to move")
+        }
+
+        switch destinationIndexPath.section {
+        case 0:
+            todos.insert(itemToMove!, at: destinationIndexPath.row)
+            itemToMove?.setValue(0, forKey: "status")
+        case 1:
+            doing.insert(itemToMove!, at: destinationIndexPath.row)
+            itemToMove?.setValue(1, forKey: "status")
+        case 2:
+            done.insert(itemToMove!, at: destinationIndexPath.row)
+            itemToMove?.setValue(2, forKey: "status")
+        default:
+            print("error in actual moving")
+            }
+        }
 
     
 }
